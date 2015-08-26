16e0e3620e09a53db3626987a8f1950b37a49564
Barbara Geller <barbara@copperspice.com>
2015-08-25 16:12:13 -0800

    Update about box

--- mdi.cpp
+++ mdi.cpp
@@ -378,7 +378,7 @@ void Mdi::on_actionAbout_triggered()
    msgB.setWindowIcon(QIcon("://resources/ks_cs.ico"));
 
    msgB.setWindowTitle(tr("About Kitchen Sink-Cs5"));
-   msgB.setText(tr("<p style=margin-right:25><center><h5>Version: 1.1<br>Build # 08.1.2015</h5></center></p>"));
+   msgB.setText(tr("<p style=margin-right:25><center><h5>Version: 1.2<br>Build # 08.25.2015</h5></center></p>"));
    msgB.setInformativeText(textBody);
 
    msgB.setStandardButtons(QMessageBox::Ok);
